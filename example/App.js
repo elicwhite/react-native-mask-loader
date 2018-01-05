6fee51b4566174ebb5ba244ef50bfa751a8afbf5
Eli White <github@eli-white.com>
2018-01-04 21:28:36 -0800

    Change root key instead of expo reload

--- example/App.js
+++ example/App.js
@@ -6,40 +6,62 @@ import Loader from 'react-native-mask-loader';
 
 type State = {|
   appReady: boolean,
+  rootKey: number,
 |};
 
 export default class App extends React.Component<{}, State> {
   state = {
     appReady: false,
+    rootKey: Math.random(),
   };
 
+  constructor() {
+    super();
+
+    this._image = require('./assets/twitter.png');
+  }
+
   componentDidMount() {
+    this.resetAnimation();
+  }
+
+  resetAnimation() {
+      this.setState({
+        appReady: false,
+        rootKey: Math.random()
+      });
+
     setTimeout(() => {
       this.setState({
         appReady: true,
       });
-    }, 2000);
+    }, 1000);
   }
 
   render() {
     return (
-      <Loader
-        isLoaded={this.state.appReady}
-        imageSource={require('./assets/twitter.png')}
-        backgroundStyle={styles.loadingBackgroundStyle}
-      >
-        <View style={styles.container}>
-          <Button onPress={() => {
-            Util.reload();
-          }} title="See it again">
-          </Button>
-        </View>
-      </Loader>
+      <View key={this.state.rootKey} style={styles.root}>
+        <Loader
+          isLoaded={this.state.appReady}
+          imageSource={this._image}
+          backgroundStyle={styles.loadingBackgroundStyle}
+        >
+          <View style={styles.container}>
+            <Button onPress={() => {
+              this.resetAnimation();
+            }} title="See it again">
+            </Button>
+          </View>
+        </Loader>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
